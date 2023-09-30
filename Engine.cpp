257e2cbd1b327a8fa289ffe80540e78d07c782e4
Stevnbak <kasper@stevnbak.dk>
2023-09-30 17:18:47 +0200

    #15 OpenGL & GLEW + #16 Window resizable

--- Engine.cpp
+++ Engine.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
-#include "SDL.h"
+#include <SDL.h> 
+#include <GL/glew.h> 
+#include <SDL_opengl.h>  
 
 using namespace std;
 
@@ -15,32 +17,49 @@ namespace StevEngine {
 	SDL_Window* window = NULL;
 	SDL_Event ev;
 
+	const GLint WIDTH = 1080, HEIGHT = 720;
+
 	void Tick(double deltaTime) {
-		cout << "Tick with deltaTime: " << deltaTime << "\n";
+		cout << "Tick with deltaTime: " << deltaTime << endl;
 	}
 	void Draw() {
-		cout << "Drawing...\n";
-		//Clear window
-		SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 255, 255, 255));
+		cout << "Drawing..." << endl;
+		// Clear the colorbuffer 
+		glClearColor(1, 1, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		//Refresh window surface
-		SDL_UpdateWindowSurface(window);
+		// draw OpenGL 
+		SDL_GL_SwapWindow(window);
 	}
 
 	void StartEngine() {
-		cout << "Started StevEngine!\n";
+		cout << "Started StevEngine!" << endl;
 		//Initialize SDL window
 		if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 			cout << "Error initializing SDL: " << SDL_GetError() << endl;
 			return;
 		}
+		//SDL & OpenGL properties
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3); SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+		glewExperimental = GL_TRUE;
 		//Create SDL window
-		window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 		// Make sure creating the window succeeded
 		if (!window) {
 			cout << "Error creating window: " << SDL_GetError() << endl;
 			return;
 		}
+		//OpenGL SDL Context
+		SDL_GLContext context = SDL_GL_CreateContext(window);
+		// Initialize GLEW to setup the OpenGL Function pointers 
+		if (glewInit() != GLEW_OK)
+		{
+			cout << "Failed to initialize GLEW"  << endl;
+			return;
+		}
+		// Define the OpenGL viewport dimensions 
+		glViewport(SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT);
 
 		// Get the surface from the window
 		winSurface = SDL_GetWindowSurface(window);
@@ -50,6 +69,8 @@ namespace StevEngine {
 			cout << "Error getting surface: " << SDL_GetError() << endl;
 			return;
 		}
+
+
 
 		//Draw window
 		Draw();
@@ -65,6 +86,16 @@ namespace StevEngine {
 						// shut down
 						running = false;
 						break;
+					case SDL_WINDOWEVENT:
+
+						if (ev.window.event == SDL_WINDOWEVENT_RESIZED) {
+							//Resized window
+							cout << "MESSAGE:Resizing window..." << endl;
+							SDL_SetWindowSize(window, ev.window.data1, ev.window.data2);
+							glViewport(0, 0, ev.window.data1, ev.window.data2);
+						}
+						break;
+
 				}
 			}
 
@@ -93,12 +124,12 @@ namespace StevEngine {
 		}
 
 		// Destroy the window. This will also destroy the surface
+		SDL_GL_DeleteContext(context);
 		SDL_DestroyWindow(window);
 		window = NULL;
 		winSurface = NULL;
 
 		// Quit SDL
 		SDL_Quit();
-
 	}
 }
