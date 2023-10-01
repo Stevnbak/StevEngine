26f1820f4aded496f3cb14f5de0c2583bb312425
Stevnbak <kasper@stevnbak.dk>
2023-10-01 13:29:13 +0200

    #4 Vector operations

--- Utilities.cpp
+++ Utilities.cpp
@@ -2,30 +2,52 @@
 #include <math.h>
 namespace StevEngine {
 	namespace Utilities {
+		Vector3d::Vector3d(double x, double y, double z) {
+			X = x;
+			Y = y;
+			Z = z;
+		}
+		Vector3d::Vector3d() {
+			X = 0;
+			Y = 0;
+			Z = 0;
+		}
 		double Vector3d::Magnitude() {
 			return sqrt(pow(X, 2) + pow(Y, 2) + pow(Z, 2));
 		}
 		double Vector3d::Distance(Vector3d target) {
 			return sqrt(pow(X - target.X, 2) + pow(Y - target.Y, 2) + pow(Z - target.Z, 2));
 		}
-		void Vector3d::Add(Vector3d vector) {
-			X += vector.X;
-			Y += vector.Y;
-			Z += vector.Z;
+		Vector3d Vector3d::operator+(const Vector3d& other) const {
+			return Vector3d(X + other.X, Y + other.Y, Z + other.Z);
 		}
-		void Vector3d::Mult(double value) {
+		Vector3d Vector3d::operator-(const Vector3d& other) const {
+			return Vector3d(X - other.X, Y - other.Y, Z - other.Z);
+		}
+		Vector3d Vector3d::operator*(const Vector3d& other) const {
+			return Vector3d(X * other.X, Y * other.Y, Z * other.Z);
+		}
+		Vector3d Vector3d::operator/(const Vector3d& other) const {
+			return Vector3d(X / other.X, Y / other.Y, Z / other.Z);
+		}
+		Vector3d Vector3d::Mult(double value) {
 			X *= value;
 			Y *= value;
 			Z *= value;
+			return Vector3d(X, Y, Z);
 		}
-		void Vector3d::Divide(double value) {
+		Vector3d Vector3d::Divide(double value) {
 			X /= value;
 			Y /= value;
 			Z /= value;
+			return Vector3d(X, Y, Z);
 		}
 		void Vector3d::Normalize() {
 			double mag = Magnitude();
 			Divide(mag);
+		}
+		Vector3d Vector3d::Cross(const Vector3d& other) const {
+			return Vector3d(Y * other.Z - Z * other.Y, - (X * other.Z - Z * other.X), X * other.Y - Y * other.X);
 		}
 	}
 }
--- Utilities.h
+++ Utilities.h
@@ -4,13 +4,19 @@ namespace StevEngine {
 	namespace Utilities {
 		class Vector3d {
 			public:
-				double X, Y, Z;
+				double X = 0, Y = 0, Z = 0;
+				Vector3d(double x, double y, double z);
+				Vector3d();
 				double Magnitude();
 				double Distance(Vector3d target);
-				void Add(Vector3d vector);
+				Vector3d operator+(const Vector3d& other) const;
+				Vector3d operator-(const Vector3d& other) const;
+				Vector3d operator*(const Vector3d& other) const;
+				Vector3d operator/(const Vector3d& other) const;
+				Vector3d Mult(double value);
+				Vector3d Divide(double value);
+				Vector3d Cross(const Vector3d& other) const;
 				void Normalize();
-				void Mult(double value);
-				void Divide(double value);
 		};
 	}
 }
