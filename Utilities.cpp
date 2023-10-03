7c4b8fbc6e3dafaa6f21b090a06ead061b5c89ef
Stevnbak <kasper@stevnbak.dk>
2023-10-03 13:32:50 +0200

    #4 Vector2d

--- Utilities.cpp
+++ Utilities.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 namespace StevEngine {
 	namespace Utilities {
+		//Vector3d
 		Vector3d::Vector3d(double x, double y, double z) {
 			X = x;
 			Y = y;
@@ -18,16 +19,16 @@ namespace StevEngine {
 		double Vector3d::Distance(Vector3d target) {
 			return sqrt(pow(X - target.X, 2) + pow(Y - target.Y, 2) + pow(Z - target.Z, 2));
 		}
-		Vector3d Vector3d::operator+(const Vector3d& other) const {
+		Vector3d Vector3d::operator+(const Vector3d & other) const {
 			return Vector3d(X + other.X, Y + other.Y, Z + other.Z);
 		}
-		Vector3d Vector3d::operator-(const Vector3d& other) const {
+		Vector3d Vector3d::operator-(const Vector3d & other) const {
 			return Vector3d(X - other.X, Y - other.Y, Z - other.Z);
 		}
-		Vector3d Vector3d::operator*(const Vector3d& other) const {
+		Vector3d Vector3d::operator*(const Vector3d & other) const {
 			return Vector3d(X * other.X, Y * other.Y, Z * other.Z);
 		}
-		Vector3d Vector3d::operator/(const Vector3d& other) const {
+		Vector3d Vector3d::operator/(const Vector3d & other) const {
 			return Vector3d(X / other.X, Y / other.Y, Z / other.Z);
 		}
 		Vector3d Vector3d::Mult(double value) {
@@ -46,8 +47,55 @@ namespace StevEngine {
 			double mag = Magnitude();
 			Divide(mag);
 		}
-		Vector3d Vector3d::Cross(const Vector3d& other) const {
-			return Vector3d(Y * other.Z - Z * other.Y, - (X * other.Z - Z * other.X), X * other.Y - Y * other.X);
+		Vector3d Vector3d::Cross(const Vector3d & other) const {
+			return Vector3d(Y * other.Z - Z * other.Y, -(X * other.Z - Z * other.X), X * other.Y - Y * other.X);
+		}
+		Vector2d Vector3d::To2D() {
+			return Vector2d(X, Y);
+		}
+		//Vector2d
+		Vector2d::Vector2d(double x, double y) {
+			X = x;
+			Y = y;
+		}
+		Vector2d::Vector2d() {
+			X = 0;
+			Y = 0;
+		}
+		double Vector2d::Magnitude() {
+			return sqrt(pow(X, 2) + pow(Y, 2));
+		}
+		double Vector2d::Distance(Vector2d target) {
+			return sqrt(pow(X - target.X, 2) + pow(Y - target.Y, 2));
+		}
+		Vector2d Vector2d::operator+(const Vector2d& other) const {
+			return Vector2d(X + other.X, Y + other.Y);
+		}
+		Vector2d Vector2d::operator-(const Vector2d& other) const {
+			return Vector2d(X - other.X, Y - other.Y);
+		}
+		Vector2d Vector2d::operator*(const Vector2d& other) const {
+			return Vector2d(X * other.X, Y * other.Y);
+		}
+		Vector2d Vector2d::operator/(const Vector2d& other) const {
+			return Vector2d(X / other.X, Y / other.Y);
+		}
+		Vector2d Vector2d::Mult(double value) {
+			X *= value;
+			Y *= value;
+			return Vector2d(X, Y);
+		}
+		Vector2d Vector2d::Divide(double value) {
+			X /= value;
+			Y /= value;
+			return Vector2d(X, Y);
+		}
+		void Vector2d::Normalize() {
+			double mag = Magnitude();
+			Divide(mag);
+		}
+		Vector3d Vector2d::To3D() {
+			return Vector3d(X, Y, 0);
 		}
 	}
 }
--- Utilities.h
+++ Utilities.h
@@ -2,6 +2,8 @@
 
 namespace StevEngine {
 	namespace Utilities {
+		class Vector2d;
+		class Vector3d;
 		class Vector3d {
 			public:
 				double X = 0, Y = 0, Z = 0;
@@ -17,6 +19,24 @@ namespace StevEngine {
 				Vector3d Divide(double value);
 				Vector3d Cross(const Vector3d& other) const;
 				void Normalize();
+				Vector2d To2D();
+		};
+
+		class Vector2d {
+		public:
+			double X = 0, Y = 0;
+			Vector2d(double x, double y);
+			Vector2d();
+			double Magnitude();
+			double Distance(Vector2d target);
+			Vector2d operator+(const Vector2d& other) const;
+			Vector2d operator-(const Vector2d& other) const;
+			Vector2d operator*(const Vector2d& other) const;
+			Vector2d operator/(const Vector2d& other) const;
+			Vector2d Mult(double value);
+			Vector2d Divide(double value);
+			void Normalize();
+			Vector3d To3D();
 		};
 	}
 }
