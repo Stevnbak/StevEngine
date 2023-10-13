de1a1a3dde4a4c9f44ffc177fbd220d08dbc213d
Stevnbak <kasper@stevnbak.dk>
2023-10-13 08:37:38 +0200

    #22 Circle fix + Capsule

--- BasicVisualComponents.cpp
+++ BasicVisualComponents.cpp
@@ -66,7 +66,7 @@ void DrawCircle() {
 	int detail = 50;
 	glBegin(GL_POLYGON);
 	float anglePerStep = (2 * M_PI) / detail; //In Radians
-	for (int i = 0; i < detail; i++)
+	for (int i = 0; i <= detail; i++)
 	{
 		float currentAngle = (anglePerStep) * i;
 		glVertex3f(0.5 * std::cos(currentAngle), 0.5 * std::sin(currentAngle), 0);
@@ -74,44 +74,11 @@ void DrawCircle() {
 	glEnd();
 }
 
-void DrawSphere() {
-	int detail = 50;
-	float anglePerStep = (2 * M_PI) / detail; //In Radians
-	//Calculate circle layers
-	std::vector<std::vector<Utilities::Vector3d>> layers;
-	for (int stack = -detail / 2; stack < detail / 2; stack++) {
-		float stackAngle = anglePerStep * stack;
-		float currentHeight = std::sin(stackAngle) * 0.5;
-		float cosStackAngle = std::cos(stackAngle);
-		std::vector<Utilities::Vector3d> circle;
-		for (int section = 0; section < detail; section++)
-		{
-			float sectionAngle = (anglePerStep) * section;
-			circle.push_back(Utilities::Vector3d(0.5 * std::cos(sectionAngle) * cosStackAngle, 0.5 * std::sin(sectionAngle) * cosStackAngle, currentHeight));
-		}
-		layers.push_back(circle);
-	}
-	//Draw polygons
-	for (int i = 0; i < layers.size() - 1; i++) {
-		std::vector<Utilities::Vector3d> circle1 = layers[i];
-		std::vector<Utilities::Vector3d> circle2 = layers[i + 1];
-		for (int j = 0; j < circle1.size() - 1; j++) {
-			float currentAngle = (anglePerStep) * i;
-			glBegin(GL_POLYGON);
-			glVertex3d(circle1[j].X, circle1[j].Y, circle1[j].Z);
-			glVertex3d(circle1[j + 1].X, circle1[j + 1].Y, circle1[j + 1].Z);
-			glVertex3d(circle2[j + 1].X, circle2[j + 1].Y, circle2[j + 1].Z);
-			glVertex3d(circle2[j].X, circle2[j].Y, circle2[j].Z);
-			glEnd();
-		}
-	}
-}
-
 void DrawCylinder() {
-	std::vector<Utilities::Vector2d> circle;
 	int detail = 50;
+	std::vector<Utilities::Vector2d> circle;
 	float anglePerStep = (2 * M_PI) / detail; //In Radians
-	for (int i = 0; i < detail; i++)
+	for (int i = 0; i <= detail; i++)
 	{
 		float currentAngle = (anglePerStep)*i;
 		circle.push_back(Utilities::Vector2d(0.5 * std::cos(currentAngle), 0.5 * std::sin(currentAngle)));
@@ -141,8 +108,104 @@ void DrawCylinder() {
 	}
 }
 
-void DrawCapsule() {
 
+void DrawSphere() {
+	int detail = 50;
+	float anglePerStep = (2 * M_PI) / detail; //In Radians
+	//Calculate circle layers
+	std::vector<std::vector<Utilities::Vector3d>> layers;
+	for (int stack = -detail / 2; stack < detail / 2; stack++) {
+		float stackAngle = anglePerStep * stack;
+		float currentHeight = std::sin(stackAngle) * 0.5;
+		float cosStackAngle = std::cos(stackAngle);
+		std::vector<Utilities::Vector3d> circle;
+		for (int section = 0; section <= detail; section++)
+		{
+			float sectionAngle = (anglePerStep)*section;
+			circle.push_back(Utilities::Vector3d(0.5 * std::cos(sectionAngle) * cosStackAngle, 0.5 * std::sin(sectionAngle) * cosStackAngle, currentHeight));
+		}
+		layers.push_back(circle);
+	}
+	//Draw polygons
+	for (int i = 0; i < layers.size() - 1; i++) {
+		std::vector<Utilities::Vector3d> circle1 = layers[i];
+		std::vector<Utilities::Vector3d> circle2 = layers[i + 1];
+		for (int j = 0; j < circle1.size() - 1; j++) {
+			float currentAngle = (anglePerStep)*i;
+			glBegin(GL_POLYGON);
+			glVertex3d(circle1[j].X, circle1[j].Y, circle1[j].Z);
+			glVertex3d(circle1[j + 1].X, circle1[j + 1].Y, circle1[j + 1].Z);
+			glVertex3d(circle2[j + 1].X, circle2[j + 1].Y, circle2[j + 1].Z);
+			glVertex3d(circle2[j].X, circle2[j].Y, circle2[j].Z);
+			glEnd();
+		}
+	}
+}
+
+void DrawCapsule() {
+	int detail = 50;
+	float anglePerStep = (2 * M_PI) / detail; //In Radians
+	//Calculate sphere layers
+	std::vector<std::vector<Utilities::Vector3d>> topLayers;
+	std::vector<std::vector<Utilities::Vector3d>> bottomLayers;
+	for (int stack = -detail / 2; stack <= detail / 2; stack++) {
+		float stackAngle = anglePerStep * stack;
+		float currentHeight = std::sin(stackAngle) * 0.25 + (stack <= 0 ? -0.25 : 0.25);
+		float cosStackAngle = std::cos(stackAngle);
+		std::vector<Utilities::Vector3d> circle;
+		for (int section = 0; section <= detail; section++)
+		{
+			float sectionAngle = (anglePerStep)*section;
+			circle.push_back(Utilities::Vector3d(0.5 * std::cos(sectionAngle) * cosStackAngle, 0.5 * std::sin(sectionAngle) * cosStackAngle, currentHeight));
+		}
+		if(stack <= 0) bottomLayers.push_back(circle);
+		if (stack > 0) topLayers.push_back(circle);
+	}
+	//Draw bottom sphere
+	for (int i = 0; i < bottomLayers.size() - 1; i++) {
+		std::vector<Utilities::Vector3d> circle1 = bottomLayers[i];
+		std::vector<Utilities::Vector3d> circle2 = bottomLayers[i + 1];
+		for (int j = 0; j < circle1.size() - 1; j++) {
+			float currentAngle = (anglePerStep)*i;
+			glBegin(GL_POLYGON);
+			glVertex3d(circle1[j].X, circle1[j].Y, circle1[j].Z);
+			glVertex3d(circle1[j + 1].X, circle1[j + 1].Y, circle1[j + 1].Z);
+			glVertex3d(circle2[j + 1].X, circle2[j + 1].Y, circle2[j + 1].Z);
+			glVertex3d(circle2[j].X, circle2[j].Y, circle2[j].Z);
+			glEnd();
+		}
+	}
+	//Draw top sphere
+	for (int i = 0; i < topLayers.size() - 1; i++) {
+		std::vector<Utilities::Vector3d> circle1 = topLayers[i];
+		std::vector<Utilities::Vector3d> circle2 = topLayers[i + 1];
+		for (int j = 0; j < circle1.size() - 1; j++) {
+			float currentAngle = (anglePerStep)*i;
+			glBegin(GL_POLYGON);
+			glVertex3d(circle1[j].X, circle1[j].Y, circle1[j].Z);
+			glVertex3d(circle1[j + 1].X, circle1[j + 1].Y, circle1[j + 1].Z);
+			glVertex3d(circle2[j + 1].X, circle2[j + 1].Y, circle2[j + 1].Z);
+			glVertex3d(circle2[j].X, circle2[j].Y, circle2[j].Z);
+			glEnd();
+		}
+	}
+	//Draw cylinder sides
+	std::vector<Utilities::Vector2d> cylinderCircle;
+	for (int i = 0; i <= detail; i++)
+	{
+		float currentAngle = (anglePerStep)*i;
+		cylinderCircle.push_back(Utilities::Vector2d(0.5 * std::cos(currentAngle), 0.5 * std::sin(currentAngle)));
+	}
+	for (int i = 0; i < cylinderCircle.size() - 1; i++) {
+		Utilities::Vector2d point1 = cylinderCircle[i];
+		Utilities::Vector2d point2 = cylinderCircle[i + 1];
+		glBegin(GL_POLYGON);
+		glVertex3f(point1.X, point1.Y, -0.25);
+		glVertex3f(point1.X, point1.Y, 0.25);
+		glVertex3f(point2.X, point2.Y, 0.25);
+		glVertex3f(point2.X, point2.Y, -0.25);
+		glEnd();
+	}
 }
 
 //Main draw function
