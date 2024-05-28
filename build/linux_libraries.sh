9c5584754612a47c71366a32d13dc33ed38db1c0
Stevnbak <kasper@stevnbak.dk>
2024-05-28 20:06:19 +0200

    Fix submodules & Build

---
+++ build/linux_libraries.sh
@@ -0,0 +1,29 @@
+COMPILER=/usr/bin/g++-13
+
+echo "SDL2 and GLEW requires access to /usr/local, so these will require being ran with sudo"
+cd ../libs
+sudo echo "Starting to build..."
+root=$(pwd)
+
+#SDL2 https://wiki.libsdl.org/SDL2/Installation#linuxunix
+cd $root/SDL2
+mkdir build
+cd build
+../configure
+make
+sudo make install
+
+#GLEW https://github.com/nigels-com/glew/tree/glew-2.2.0?tab=readme-ov-file#linux-and-mac
+cd $root/GLEW
+make extensions PYTHON=python3
+make CFLAGS.EXTRA="-DGLEW_STATIC"
+sudo make install
+
+#Jolt
+cd $root/JoltPhysics
+cd Build
+cmake -S . -B Linux_Debug -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=$COMPILER "${@}" -DGENERATE_DEBUG_SYMBOLS=ON -DDEBUG_RENDERER_IN_DEBUG_AND_RELEASE=OFF -DPROFILER_IN_DEBUG_AND_RELEASE=OFF -DENABLE_OBJECT_STREAM=OFF
+cd Linux_Debug && make -j 8 && ./UnitTests
+
+# Return to ./build
+cd $root/../build
